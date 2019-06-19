b7780f41118992172b586a6c18a604b8958d58d7
muhammad-taha1 <muhammad.taha@mail.mcgill.ca>
2019-06-19 01:16:07 -0400

    use es2015

--- element-build.js
+++ element-build.js
@@ -2,10 +2,10 @@ const fs = require('fs-extra');
 const concat = require('concat');
 (async function build() {
   const files = [
-    './dist/runtime-es5.js',
-    './dist/polyfills-es5.js',
+    './dist/runtime-es2015.js',
+    './dist/polyfills-es2015.js',
     './dist/scripts.js',
-    './dist/main-es5.js',
+    './dist/main-es2015.js',
   ]
   await fs.ensureDir('elements')
   await concat(files, 'elements/component-A.js');
